3bec869ce02c9df1cd8ff1f224361f86345c2c82
Sudir Krishnaa RS <Sudir.Krishnaa@albertsons.com>
2024-01-19 11:43:35 +0530

    Add parcel , React and ReactDOM dependencies in EP-02 code

---
+++ E02 - Igniting our App/Code files/dist/index.6bd02f5a.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("h1", {
+    id: "head-test-1"
+}, "Hello World from React!");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
