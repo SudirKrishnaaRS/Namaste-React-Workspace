58f40e3fc8c82f92e7471f199e7ad4983e6be4e9
Sudir Krishnaa RS <Sudir.Krishnaa@albertsons.com>
2024-01-19 11:44:39 +0530

    Using React in our app

--- E02 - Igniting our App/Code files/App.js
+++ E02 - Igniting our App/Code files/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const heading = React.createElement("h1", {id: "head-test-1"}, "Hello World from React!");
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
