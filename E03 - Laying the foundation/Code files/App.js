52f092d0381efad7cd1bc9ee3c947383fe3541c1
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-08-22 14:35:43 +0530

    Ep03 Intro

--- E03 - Laying the foundation/Code files/App.js
+++ E03 - Laying the foundation/Code files/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id: "head1"}, "Namaste React");
+const heading = React.createElement("h1", { id: "head1" }, "Namaste React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
